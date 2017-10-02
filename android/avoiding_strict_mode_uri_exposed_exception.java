0fcac0eee76d6fc936eefa2714718872b1b1adee
Abul Hasnat Nayeem <nayeem@softbdltd.com>
2017-10-02 10:27:44 +0600

    Add empty line EOF

--- android/avoiding_strict_mode_uri_exposed_exception.java
+++ android/avoiding_strict_mode_uri_exposed_exception.java
@@ -1,4 +1,8 @@
-//Place these lines in onCreate method of any activity
+// Place these lines in onCreate method of any activity
 
 StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
 StrictMode.setVmPolicy(builder.build()); //ignore URI Exposer exception
+
+
+
+

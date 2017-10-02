916944c0beddf04668d53f9dac839d72077870d4
Abul Hasnat Nayeem <nayeem@softbdltd.com>
2017-10-02 12:30:18 +0600

    Improved location access checking in android

--- android/check_location_access.java
+++ android/check_location_access.java
@@ -2,3 +2,33 @@ public static boolean isLocationEnabled(Context mContext) {
     LocationManager locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
     return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 }
+
+public static void checkLocationAccess(Context mContext) {
+    if (!isLocationEnabled(mContext)) {
+        showGpsSettingAlert(mContext, true);
+    }
+}
+
+public static void showGpsSettingAlert(final Context mContext){
+    AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+    
+    alertDialog.setTitle("GPS"); // Setting Dialog Title
+    alertDialog.setCancelable(true); // Dialog will be closed if touched outside, if set to false dialog cant be closed until cancel button is pressed
+
+    alertDialog.setMessage("GPS is not enabled. Please got to setting to enable it.");
+
+    alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog,int which) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            mContext.startActivity(intent);
+        }
+    });
+
+    alertDialog.setNegativeButton("বাতিল", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+        }
+    });
+
+    alertDialog.show();
+}
